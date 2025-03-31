f7cd9c77408d13291b44113f8108d81ea237422c
Peparon <peparoncino@gmail.com>
2025-03-31 07:25:51 +0000

    routes commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'messages/create'
-  get 'rooms/index'
-  get 'rooms/show'
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'rooms#index'
+
+  resources :rooms, only: [:index, :show, :create] do
+    resources :messages, only: [:create]
+  end
+  resources :users, only: [:show]
 end
