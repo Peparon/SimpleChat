69608f98aadc115e85221a7b6f2e5904526cc1b9
Peparon <peparoncino@gmail.com>
2025-04-02 12:53:36 +0000

    routes fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'rooms#index'
-
-  resources :rooms, only: [:index, :show, :create] do
-    resources :messages, only: [:index, :create]
+  
+  resources :users, only: [:show, :edit, :update] do
+    resources :rooms, only: [:show, :create]
   end
-  resources :users, only: [:show, :edit, :update]
+
+  resources :rooms, only: [:index] do
+    resources :messages, only: [:create]
+  end
+  
 end
