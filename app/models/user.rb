e591b6297823296bca80901f3c51c43854296405
Peparon <peparoncino@gmail.com>
2025-05-06 13:17:41 +0000

    user edit update

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,9 +17,14 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 10 }
 
   before_create :generate_unique_friend_id
+  after_create :create_sample_chat_data
 
   def approved_friends
     friends.merge(Friendship.where(status: 'approved')) + inverse_friends.merge(Friendship.where(status: 'approved'))
+  end
+
+  def room_with(other_user)
+    Room.joins(:room_users).where(room_users: { user_id: [self.id, other.id] }).group('rooms.id').having('COUNT(DISTINCT room_users.user_id) = 2').first
   end
 
   private
@@ -31,4 +36,30 @@ class User < ApplicationRecord
     end
   end
 
+  # サンプルチャット機能一旦保留
+  # def create_sample_chat_data
+  #   sample = User.find_by(email: 'sample@sample.com')
+  #   return if sample.nil? || sample == self
+
+  #   Friendship.find_or_create_by(user: self, friend: sample) { |f| f.status = :approved }
+  #   Friendship.find_or_create_by(user: sample, friend: self) { |f| f.status = :approved }
+
+  #   existing_room = Room.joins(:room_users)
+  #                       .where(room_users: { user_id: [self.id, sample.id] })
+  #                       .group('rooms.id')
+  #                       .having('COUNT(DISTINCT room_users.user_id) = 2')
+  #                       .first
+
+  #   room = existing_room || Room.create!
+  #   RoomUser.find_or_create_by(user: self, room: room)
+  #   RoomUser.find_or_create_by(user: sample, room: room)
+
+  #   if room.messages.empty?
+  #     Message.create!(
+  #       room: room,
+  #       user: sample,
+  #       content: 'SimpleChatへようこそ！友人IDで友人を検索して、チャットを始めましょう！'
+  #     )
+  #   end
+  # end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# サンプルチャット機能一旦保留
+# User.find_or_create_by(email: 'sample@sample.com') do |user|
+#   user.name = 'サンプル'
+#   user.password = 'samplepassword'
+#   user.password_confirmation = 'samplepassword'
+# end
