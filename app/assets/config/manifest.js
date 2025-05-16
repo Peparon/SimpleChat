c7f3b7b98869ce8f5a6543c895e8d31623eb0b43
Peparon <peparoncino@gmail.com>
2025-05-16 13:46:33 +0000

    manifest.js update

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,8 @@
 //= link_tree ../images
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
+
+//= link yorkie_stay.png
+//= link yorkie_walk.png
+//= link seadog.jpeg
+//= link paint_mask.png
