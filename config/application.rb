f64445c04248ada65e9694470c3308b77baf1b49
Peparon <peparoncino@gmail.com>
2025-05-28 08:51:28 +0000

    timezone tokyo fix

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,10 @@ module Simplechat
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    # 日本時間に設定
+    config.time_zone = 'Tokyo' # 表示時刻を日本時間に
+    config.active_record.default_timezone = :local # DB保存も日本時間に
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
