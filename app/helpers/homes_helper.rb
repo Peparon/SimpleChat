b54c4414005203cc3f1da363f06d604e9b3e0f38
Peparon <peparoncino@gmail.com>
2025-06-07 13:28:03 +0000

    mobile size rooms/_index scroll lock

--- app/helpers/homes_helper.rb
+++ app/helpers/homes_helper.rb
@@ -1,2 +1,8 @@
 module HomesHelper
+    def no_scroll_lock?
+        params[:mode] == "friends" && !@show_friend_search
+    end
 end
+# URLパラメータが?mode=friendsの時
+# インスタンス変数@show_friend_searchがfalseまたはnilの時
+# 上記の二つを満たす時だけスクロール
